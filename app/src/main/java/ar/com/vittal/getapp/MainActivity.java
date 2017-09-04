d1c83ff4343e3c8ae292c84d2aa260337eac918f
matsebman <mati.seb@hotmail.com>
2017-09-04 11:59:11 -0300

    + Agregada pantalla principal + Agregadas tabs de home, buscador e informacion (sin funcionalidad) + Agregados botones que dirijen a la pantalle de mapa

---
+++ app/src/main/java/ar/com/vittal/getapp/MainActivity.java
@@ -0,0 +1,64 @@
+package ar.com.vittal.getapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    public final static String NEAREST_LATLONG = "NEAREST_LATLONG";
+    public final static String ALL_LATLONG = "ALL_LATLONG";
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    //mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    //mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    //mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    public void goToNearestDEA(View view)
+    {
+        Intent intent = new Intent(this, MapsActivity.class);
+        intent.putExtra(NEAREST_LATLONG, 0);
+        startActivity(intent);
+    }
+
+    public void listAllNearestDEAS(View view)
+    {
+        Intent intent = new Intent(this, MapsActivity.class);
+        intent.putExtra(ALL_LATLONG, 0);
+        startActivity(intent);
+    }
+
+}
