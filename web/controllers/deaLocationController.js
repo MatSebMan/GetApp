28e8e05e07133cf325ae71db56af3468d387fd64
Damian <damianfiuba@gmail.com>
2017-10-30 03:43:22 -0300

    Estados y distancia max

--- web/controllers/deaLocationController.js
+++ web/controllers/deaLocationController.js
@@ -8,7 +8,7 @@ exports.create = function(req, res) {
     if(!req.body.zona_protegida || !req.body.latitud || !req.body.longitud )
         res.send(500);
     db.connect("getapp", function(client) {
-        const queryString = 'INSERT INTO dea(zona_protegida,location) VALUES($1,ST_MakePoint($3,$2))';
+        const queryString = 'INSERT INTO dea(zona_protegida,location,activo) VALUES($1,ST_MakePoint($3,$2),false)';
         const values = [];
         values.push(req.body.zona_protegida);
         values.push(req.body.latitud);
@@ -31,7 +31,7 @@ exports.findNearestDeas = function(req, res) {
         if(!req.query.cantidad || !req.query.latitud || !req.query.longitud )
             res.send(500, "Parametros incorrectos");
         var queryString = 
-        'SELECT id,zona_protegida as nombre, ST_Y(location) as latitud, ST_X(location) as longitud FROM dea ORDER BY location <-> st_makepoint($2,$1) LIMIT $3';
+        'SELECT id,zona_protegida as nombre, ST_Y(location) as latitud, ST_X(location) as longitud FROM dea WHERE ST_Distance_Sphere(location,st_makepoint($2,$1))<1000 ORDER BY location <-> st_makepoint($2,$1) LIMIT $3';
         console.log(queryString);
         const values = [];
         values.push(req.query.latitud);
@@ -53,7 +53,7 @@ exports.findAll = function(req, res) {
     console.log('GET/deaList');
     var success = function(client) {
         var queryString = 
-        'SELECT id as "Id",zona_protegida as "Nombre", ST_Y(location) as "Latitud", ST_X(location) as "Longitud", \'\' as "Fecha de Entrega" FROM dea ORDER BY id';
+        'SELECT id as "Id",zona_protegida as "Nombre", ST_Y(location) as "Latitud", ST_X(location) as "Longitud", activo as "Activo", \'\' as "Fecha de Entrega" FROM dea ORDER BY id';
         client.query(queryString)
         .then(dbRes => {
             db.disconnect(client)
@@ -74,7 +74,7 @@ exports.findById = function(req, res) {
     console.log('GET/deaList/'+req.params.id);
     var success = function(client) {
         var queryString = 
-        'SELECT id as "Id",zona_protegida as "Nombre Zona Protegida", ST_Y(location) as "Latitud", ST_X(location) as "Longitud", \'\' as "Fecha de Entrega" FROM dea WHERE id = $1';
+        'SELECT id as "Id",zona_protegida as "Nombre Zona Protegida", ST_Y(location) as "Latitud", ST_X(location) as "Longitud",activo as "Activo", en_uso as "En Uso", \'\' as "Fecha de Entrega" FROM dea WHERE id = $1';
         const values = [];
         values.push(req.params.id);
         client.query(queryString, values)
@@ -98,12 +98,14 @@ exports.edit = function(req, res) {
     console.log(req.body);
     db.connect("getapp", function(client) {
         // Delete query (parametrizada para evitar sql injection)
-        var queryString = 'UPDATE dea SET (zona_protegida,location) = ($2,ST_MakePoint($4,$3)) WHERE id = $1';
+        var queryString = 'UPDATE dea SET (zona_protegida,location,activo,en_uso) = ($2,ST_MakePoint($4,$3),$5,$6) WHERE id = $1';
         const values = [];
         values.push(req.params.id);
         values.push(req.body.zona_protegida);
         values.push(req.body.latitud);
         values.push(req.body.longitud);
+        values.push(req.body.activo);
+        values.push(req.body.en_uso);
         client.query(queryString, values).then(dbRes => {
             db.disconnect(client)
             res.status(200).send();
--- web/public/app/components/main/deas/admin/campos-creacion-deas.ts
+++ web/public/app/components/main/deas/admin/campos-creacion-deas.ts
@@ -2,11 +2,15 @@ export var creacionDea = {
     types: {
         zona_protegida: 'text',
         latitud: 'posicion',
-        longitud: 'posicion'
+        longitud: 'posicion',
+        activo: 'boolean',
+        en_uso: 'boolean'
     },
     text: {
         zona_protegida: 'Nombre Zona Protegida',
         latitud: 'Latitud',
-        longitud: 'Longitud'
+        longitud: 'Longitud',
+        activo: 'Activo',
+        en_uso: 'En Uso'
     }    
 }
